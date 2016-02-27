d7019d53b3f8e9fe526a8cbd06944bbca49df07f
mason91 <oblivion.mason@gmail.com>
2016-02-27 12:49:24 +0800

    Update mainProg.c

--- mainProg.c
+++ mainProg.c
@@ -1,6 +1,7 @@
 #include "xmk.h"
 #include "sys/init.h"
 #include "xgpio.h"
+#include "pthread.h"
 #include "xuartlite.h"
 #include "xparameters.h"
 #include <sys/timer.h> //for using sleep. need to set config_time to true
@@ -11,10 +12,13 @@
 #define XST_FAILURE                     1L
 // TO DO : read and understand the xilkernel system calls as well as the driver APIs used.
 
+pthread_t tid1;
 const int LEFT_BTN = 4;
 const int RIGHT_BTN = 8;
+const int LENGTH = 80;
+const int DIST=10;
 XGpio gpPB; //PB device instance.
-long last_int_time = 0;
+long last_int_time1 = 0, last_int_time0 = 0;
 int btn;
 int x=310;
 
@@ -25,9 +29,24 @@ static void gpPBIntHandler(void *arg) //Should be very short (in time). In a pra
 	//clear the interrupt flag. if this is not done, gpio will keep interrupting the microblaze.--
 	// --Possible to use (XGpio*)arg instead of &gpPB
 	XGpio_InterruptClear(&gpPB,1);
-	//Read the state of the push buttons.
-	btn = XGpio_DiscreteRead(&gpPB, 1);
-	xil_printf("PB event, val = %d \r\n", btn); // for testing.	
+	long int_time = xget_clock_ticks();
+	val = XGpio_DiscreteRead(&gpPB, 1);
+	if (val>0) {
+		if (int_time - last_int_time1>20 && int_time - last_int_time0> 20)
+		{
+			//Read the state of the push buttons.
+			btn = val;
+		}
+		last_int_time1 = int_time;
+	}
+	else {
+		if (int_time - last_int_time0 > 20)
+		{
+			//Read the state of the push buttons.
+			btn = val;
+		}
+		last_int_time0 = int_time;
+	}
 }
 
 void moveBar()
@@ -35,14 +54,18 @@ void moveBar()
 	if (btn==LEFT_BTN)
 	{
 		//Decrement x coordinate
-		x=x-5;
-		drawBar(5,20,400,x);
+		x=x-DIST;
+		if (x<0) x=0;
+		drawBar(5,LENGTH,409,x,0x000000ff);
+		drawBar(5,DIST,409,x+LENGTH,0x00ffffff);
 	}
 	else if (btn==RIGHT_BTN)
 	{
 		//Increment x coordinate
-		x=x+5
-		drawBar(5,20,400,x);
+		x=x+DIST;
+		if (x>639-LENGTH) x=639-LENGTH;
+		drawBar(5,LENGTH,409,x,0x000000ff);
+		drawBar(5,DIST,409,x-DIST,0x00ffffff);
 	}
 }
 
@@ -61,7 +84,7 @@ void main_prog(void *arg)
 	int ret;
 
 	xil_printf("Initializing PB\r\n");
-	 initTFT();
+	initTFT();
 	xil_printf("Finish initializing TFT\r\n");
 	fillScreen();
 	// Initialise the PB instance
@@ -70,7 +93,7 @@ void main_prog(void *arg)
 	XGpio_SetDataDirection(&gpPB, 1, 0x000000FF);
 
 	xil_printf("Enabling PB interrupts\r\n");
-	 //global enable
+	//global enable
 	XGpio_InterruptGlobalEnable(&gpPB);
 	// interrupt enable. both global enable and this function should be called to enable gpio interrupts.
 	XGpio_InterruptEnable(&gpPB,1);
@@ -78,31 +101,31 @@ void main_prog(void *arg)
 	register_int_handler(XPAR_MICROBLAZE_0_AXI_INTC_AXI_GPIO_0_IP2INTC_IRPT_INTR, gpPBIntHandler, &gpPB);
 	//enable the interrupt in xilkernel
 	enable_interrupt(XPAR_MICROBLAZE_0_AXI_INTC_AXI_GPIO_0_IP2INTC_IRPT_INTR);
-
-	 //start thread 1
+	drawBar(5,LENGTH,409,x, 0x000000ff);
+	//start thread 1
 	ret = pthread_create (&tid1, NULL, (void*)thread_func_1, NULL);
 	if (ret != 0) {
-    xil_printf ("-- ERROR (%d) launching thread_func_1...\r\n", ret);
+		xil_printf ("-- ERROR (%d) launching thread_func_1...\r\n", ret);
 	}
 	else {
-    xil_printf ("Thread 1 launched with ID %d \r\n",tid1);
+		xil_printf ("Thread 1 launched with ID %d \r\n",tid1);
 	}
 }
 
 int main()
 {
-	  print("-- Entering main() --\r\n");
-	  //Initialize Xilkernel
-	  xilkernel_init();
+	print("-- Entering main() --\r\n");
+	//Initialize Xilkernel
+	xilkernel_init();
 
-	  //Add main_prog as the static thread that will be invoked by Xilkernel
-	  xmk_add_static_thread(main_prog, 0);
+	//Add main_prog as the static thread that will be invoked by Xilkernel
+	xmk_add_static_thread(main_prog, 0);
 
-	  //Start Xilkernel
-	  xilkernel_start();
+	//Start Xilkernel
+	xilkernel_start();
 
-	  /* Never reached */
-	  return 0;
+	/* Never reached */
+	return 0;
 }
 
 
